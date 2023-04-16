0cb35bf39939c153ac3d80c2814a324c8a37b769
REBELBOT <78189163+DARKWEBREBEL@users.noreply.github.com>
2023-04-17 01:56:02 +0530

    Add files via upload

---
+++ exampleconfig.py
@@ -0,0 +1,13 @@
+from sample_Config import Config
+
+class Development(Config):
+  APP_ID = 6
+  API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
+  ALIVE_NAME = "YOUR VALUE"
+  DB_URI = "Your value"
+  STRING_SESSION = "Your value"
+  BOT_TOKEN = "Your value"
+  BOT_USERNAME = "Your value"
+  HNDLR = "."
+  SUDO_USERS = []
+  HNDLR = "."
